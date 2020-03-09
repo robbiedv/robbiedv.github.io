939a0037e2de87981d5f18880a3833c4fc3eec21
robbiedv <robbie.c.devries@gmail.com>
2020-03-09 09:42:51 -0500

    emma

---
+++ master.js
@@ -0,0 +1,20 @@
+//when the data bitton is clicked
+  //designer fly off screen left
+  //developer fly off screen right
+  //move data up increase space between letters
+  //grow a text box
+  //data description fly up and fade in
+
+  //when the designer bitton is clicked
+    //data fly off screen left
+    //developer fly off screen right
+    //move designer up increase space between letters
+    //grow a text box
+    //designer description fly up and fade in
+
+    //when the developer bitton is clicked
+      //data fly off screen left
+      //designer fly off screen right
+      //move developer up increase space between letters
+      //grow a text box
+      //developer description fly up and fade in
