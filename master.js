eb4a0c9a29c17140a99f0f34832bdd01c4730403
RobbieD <robbie@upperlimitsinc.com>
2020-03-11 16:02:30 -0500

    style updates + JS structure started

--- master.js
+++ master.js
@@ -1,28 +1,56 @@
-//when the data button is clicked
 window.onload = function() {
-const dataButton = document.getElementById('data-button')
 
-// dataButton.addEventListener('click', function() {
-//   dataButton.classList.add('data-activate')
+// access menu buttons
+const buttons = document.getElementsByClassName('button');
 
-  // dataButton.classList.add('data-activate')
-  //designer fly off screen left
-  //developer fly off screen right
-  //move data up increase space between letters
-  //grow a text box
-  //data description fly up and fade in
+const devButton = document.getElementById('dev');
+const dataButton = document.getElementById('data');
+const dadButton = document.getElementById('dad');
 
-  //when the designer button is clicked
+// loop through list, select each one by innerHTML
+for (let i=0; i < buttons.length; i++) {
+  buttons[i].onclick = function() {
+    if (buttons[i].innerHTML == "DEVELOPER") {
+      dev(buttons[i]);
+    } else if (buttons[i].innerHTML == "DATA") {
+      data(buttons[i]);
+    } else {
+      dad(buttons[i]);
+    }
+  }
+}
+
+
+
+function dev(x) {
     //data fly off screen left
+    console.log(x)
+    //designer fly off screen right
+    //move developer up increase space between letters
+    //grow a text box
+    //developer description fly up and fade in
+}
+
+function data(x) {
+  console.log(x)
+  //when the data button is clicked
+    //dad fly off screen left
     //developer fly off screen right
-    //move designer up increase space between letters
+    //move data up increase space between letters
     //grow a text box
     //designer description fly up and fade in
-
-    //when the developer button is clicked
-      //data fly off screen left
-      //designer fly off screen right
-      //move developer up increase space between letters
-      //grow a text box
-      //developer description fly up and fade in
 }
+
+function dad(x) {
+  console.log(x)
+  //when the dad button is clicked
+    //data fly off screen left
+    //developer fly off screen right
+    //move dad up increase space between letters
+    //grow a text box
+    //developer description fly up and fade in
+}
+
+
+
+};
