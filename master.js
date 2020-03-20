f2e4b06ac28ca7e9196e70de0894522a63a409ba
robbiedv <robbie.c.devries@gmail.com>
2020-03-20 11:04:40 -0500

    commit before JS refactoring

--- master.js
+++ master.js
@@ -1,62 +1,123 @@
-window.onload = function() {
-
-const buttons = document.getElementsByClassName('button');
 
 
-const devButton = document.getElementById('dev');
-const dataButton = document.getElementById('data');
-const dadButton = document.getElementById('dad');
+window.onload = function () {
 
-// loop through menu list, select each by innerHTML
-for (let i=0; i < buttons.length; i++) {
-  buttons[i].onclick = function() {
-    if (buttons[i].innerHTML == "DEVELOPER") {
-      dev(buttons[i]);
-    } else if (buttons[i].innerHTML == "DATA") {
-      data(buttons[i]);
+  // MENU BUTTONS //
+
+  var buttons = document.getElementsByClassName('button');
+  var devButton = document.getElementById('dev');
+  var dataButton = document.getElementById('data');
+  var dadButton = document.getElementById('dad');
+
+  // SKILL TREE ID'S //
+
+  var devGrid = document.getElementById('dev-grid');
+  var dataGrid = document.getElementById('data-grid');
+  var dadGrid = document.getElementById('dad-grid');
+
+  // CLOSE BUTTON //
+
+  let closeButton = document.getElementById('close-button');
+  var gridText = document.getElementsByTagName('text');
+
+  // loop through menu list, select each by innerHTML
+  var _loop = function _loop(_i) {
+    // closeButton.classList.toggle('show-hide');
+    buttons[_i].onclick = function () {
+      if (buttons[_i].innerHTML == "DEVELOPER") {
+        dev(buttons[_i]);
+      } else if (buttons[_i].innerHTML == "DATA") {
+        data(buttons[_i]);
+      } else {
+        dad(buttons[_i]);
+      }
+    };
+  };
+
+  for (var _i = 0; _i < buttons.length; _i++) {
+    _loop(_i);
+  }
+
+  var devClicked = false;
+  var dataClicked = false;
+  var dadClicked = false;
+
+  // CLOSE BUTTON FUNCTION //
+
+  closeButton.onclick = function () {
+    // closeButton.classList.toggle('show-hide');
+    console.log(devClicked, dataClicked, dadClicked);
+    if (devClicked = true) {
+      devButton.classList.remove('fade-out');
+      dataButton.classList.remove('slide-left');
+      dadButton.classList.remove('slide-right');
+      devGrid.classList.add('hide-grid');
+      devGrid.classList.remove('draw-grid');
+      devClicked = false;
+    } else if (dataClicked = true) {
+      console.log('ya crazy animal')
+      dataButton.classList.remove('fade-out');
+      dadButton.classList.remove('slide-left');
+      devButton.classList.remove('slide-right');
+      dataGrid.classList.add('hide-grid');
+      dataGrid.classList.remove('draw-grid');
+      dataClicked = false;
+    } else if (dadClicked = true) {
+      console.log('lets go');
+      dadButton.classList.remove('fade-out');
+      dataButton.classList.remove('slide-left');
+      devButton.classList.remove('slide-right');
+      dadGrid.classList.add('hide-grid');
+      dadGrid.classList.remove('draw-grid');
+      dadClicked = false;
     } else {
-      dad(buttons[i]);
+      console.log('Garbage!!!!!!')
+    }
+  };
+
+  function dev(x) {
+    devClicked = true;
+    console.log(devClicked);
+    devButton.classList.add('fade-out');
+    dataButton.classList.add('slide-left');
+    dadButton.classList.add('slide-right');
+    devGrid.classList.remove('hide-grid');
+    devGrid.classList.add('draw-grid');
+
+    for (i = 0; i < gridText.length; i++) {
+      if (gridText[i].classList == "dev") {
+        gridText[i].classList.add('grid-text');
+      }
     }
   }
-}
 
-// get each grid
+  function data(x) {
+    dataClicked = true;
+    dataButton.classList.add('fade-out');
+    dadButton.classList.add('slide-left');
+    devButton.classList.add('slide-right');
+    dataGrid.classList.remove('hide-grid');
+    dataGrid.classList.add('draw-grid');
 
-//select matching grid to button clicked
-//toggle draw-grid class 
+    for (i = 0; i < gridText.length; i++) {
+      if (gridText[i].classList == "data") {
+        gridText[i].classList.add('grid-text');
+      }
+    }
+  }
 
+  function dad(x) {
+    dadClicked = true;
+    dadButton.classList.add('fade-out');
+    dataButton.classList.add('slide-left');
+    devButton.classList.add('slide-right');
+    dadGrid.classList.remove('hide-grid');
+    dadGrid.classList.add('draw-grid');
 
-
-function dev(x) {
-  //fade out dev
-  devButton.classList.toggle('fade-out');
-  dataButton.classList.toggle('slide-left');
-  //designer fly off screen right
-  dadButton.classList.toggle('slide-right');
-  //developer description fly up and fade in
-}
-
-function data(x) {
-  //fade out data
-  dataButton.classList.toggle('fade-out');
-  //dad fly off screen left
-  dadButton.classList.toggle('slide-left')
-  //developer fly off screen right
-  devButton.classList.toggle('slide-right')
-  //designer description fly up and fade in
-}
-
-function dad(x) {
-  //fade out dad
-  dadButton.classList.toggle('fade-out');
-  //data fly off screen left
-  dataButton.classList.toggle('slide-left');
-  //developer fly off screen right
-  devButton.classList.toggle('slide-right');
-  //developer description fly up and fade in
-}
-
-
-
-
+    for (i = 0; i < gridText.length; i++) {
+      if (gridText[i].classList == "dad") {
+        gridText[i].classList.add('grid-text');
+      }
+    }
+  }
 };
