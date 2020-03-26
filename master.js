91f8e92ddad9bade9d19fd1545fc66d4cf8006cc
robbiedv <robbie.c.devries@gmail.com>
2020-03-25 19:56:49 -0500

    pod style fixes

--- master.js
+++ master.js
@@ -1,228 +1,223 @@
-window.onload = function() {
+"use strict";
 
-    // MENU BUTTONS //
+window.onload = function () {
 
-    const menuButtons = document.getElementsByClassName('button');
-    const devButton = document.getElementById('dev');
-    const dataButton = document.getElementById('data');
-    const dadButton = document.getElementById('dad');
+  // MENU BUTTONS //
+  
+  var menuButtons = document.getElementsByClassName('button');
+  var devButton = document.getElementById('dev');
+  var dataButton = document.getElementById('data');
+  var dadButton = document.getElementById('dad');
 
-    // PODS //
+  // PODS //
 
-    const mainPod = document.getElementById('main-pod');
-    const mainPodText = document. getElementById('main-pod-text');
+  var mainPod = document.getElementById('main-pod');
+  var mainPodText = document.getElementById('main-pod-text');
 
-    const pod1 = document.getElementById('pod-1');
-    const pod2 = document.getElementById('pod-2');
-    const pod3 = document.getElementById('pod-3');
-    const pod4 = document.getElementById('pod-4');
-    const pod5 = document.getElementById('pod-5');
-    const pod6 = document.getElementById('pod-6');
+  var pod1 = document.getElementById('pod-1');
+  var pod2 = document.getElementById('pod-2');
+  var pod3 = document.getElementById('pod-3');
+  var pod4 = document.getElementById('pod-4');
+  var pod5 = document.getElementById('pod-5');
+  var pod6 = document.getElementById('pod-6');
 
-    const pod1Text = document.getElementById('pod1-text');
-    const pod2Text = document.getElementById('pod2-text');
-    const pod3Text = document.getElementById('pod3-text');
-    const pod4Text = document.getElementById('pod4-text');
-    const pod5Text = document.getElementById('pod5-text');
-    const pod6Text = document.getElementById('pod6-text');
+  var pod1Text = document.getElementById('pod1-text');
+  var pod2Text = document.getElementById('pod2-text');
+  var pod3Text = document.getElementById('pod3-text');
+  var pod4Text = document.getElementById('pod4-text');
+  var pod5Text = document.getElementById('pod5-text');
+  var pod6Text = document.getElementById('pod6-text');
 
-    const devPods = [pod1, pod2, pod3, pod4, pod5, pod6];
-    const dataPods = [pod1, pod3, pod4];
-    const dadPods = [pod1, pod2, pod4, pod5];
+  var devPods = [pod1, pod2, pod3, pod4, pod5, pod6];
+  var dataPods = [pod1, pod3, pod4];
+  var dadPods = [pod1, pod2, pod4, pod5];
 
-    // const devPodText = [pod1Text, pod2Text, pod3Text, pod4Text, pod5Text, pod6Text];
-    // const dataPodText = [pod1Text, pod3Text, pod4Text];
-    // const dadPodText = [pod1Text, pod2Text, pod4Text, pod5Text];
+  // CLOSE BUTTON //
 
-    // CLOSE BUTTON //
+  var closeButton = document.getElementById('close-button');
 
-    const closeButton = document.getElementById('close-button');
+  // FUNCTIONS FOR ADDING + REMOVING CLASSES
 
-    // FUNCTIONS FOR ADDING + REMOVING CLASSES
+  function addClass(elementName, className) {
+    elementName.classList.add(className);
+  }
 
-    function addClass(elementName, className) {
-        elementName.classList.add(className)
-    }
+  function removeClass(elementName, className) {
+    elementName.classList.remove(className);
+  }
 
-    function removeClass(elementName, className) {
-        elementName.classList.remove(className)
-    }
+  var devClicked = false;
+  var dataClicked = false;
+  var dadClicked = false;
 
-    let devClicked = false;
-    let dataClicked = false;
-    let dadClicked = false;
-
-    for (let i = 0; i < menuButtons.length; i++) {
-        menuButtons[i].onclick = function() {
-            if (menuButtons[i].innerHTML == 'DEVELOPER') {
-                devMenu();
-                devClicked = true;
-            } else if (menuButtons[i].innerHTML == 'DATA') {
-                dataMenu();
-                dataClicked = true;
-            } else if (menuButtons[i].innerHTML == 'DAD') {
-                dadMenu();
-                dadClicked = true;
-            } else {
-                console.log('menu button error')
-            }
-        }
-    }
-
-    // MENU ANIMATIONS //
-
-    function devMenu() {
-        console.log('dev button was clicked')
-        showDevPods();
-        devButton.classList.add('fade-out');
-        dataButton.classList.add('slide-left');
-        dadButton.classList.add('slide-right');
-        removeClass(closeButton, 'hide');
-        closeButton.classList.add('show');
-    }
-
-    function dataMenu() {
-        console.log('the data button was clicked');
-        showDataPods();
-        dataButton.classList.add('fade-out');
-        devButton.classList.add('slide-left');
-        dadButton.classList.add('slide-right');
-        closeButton.classList.remove('hide');
-        closeButton.classList.add('show');
-    }
-
-    function dadMenu() {
-        console.log('the dad button was clicked');
-        showDadPods();
-        dadButton.classList.add('fade-out');
-        devButton.classList.add('slide-left');
-        dataButton.classList.add('slide-right');
-        closeButton.classList.remove('hide');
-        closeButton.classList.add('show');
-    }
-
-    // SKILL POD FUNCTIONS //
-
-    // DEVELOPER //
-
-    function showDevPods() {
-
-      for (let i = 0; i < devPods.length; i++) {
-        removeClass(devPods[i], 'hide');
-        addClass(devPods[i], 'show');
+  var _loop = function _loop(i) {
+    menuButtons[i].onclick = function () {
+      if (menuButtons[i].innerHTML == 'DEVELOPER') {
+        devMenu();
+        devClicked = true;
+      } else if (menuButtons[i].innerHTML == 'DATA') {
+        dataMenu();
+        dataClicked = true;
+      } else if (menuButtons[i].innerHTML == 'DAD') {
+        dadMenu();
+        dadClicked = true;
+      } else {
+        console.log('menu button error');
       }
-      addClass(mainPod, 'show')
-      removeClass(mainPod, 'hide');
+    };
+  };
 
-      mainPodText.innerHTML = 'DEV';
-      pod1Text.innerHTML = 'HTML';
-      pod2Text.innerHTML = 'CSS';
-      pod3Text.innerHTML = 'JS';
-      pod4Text.innerHTML = 'GIT';
-      pod5Text.innerHTML = 'REACT';
-      pod6Text.innerHTML = 'LINUX';
+  for (var i = 0; i < menuButtons.length; i++) {
+    _loop(i);
+  }
+
+  // MENU ANIMATIONS //
+
+  function devMenu() {
+    console.log('dev button was clicked');
+    showDevPods();
+    devButton.classList.add('fade-out');
+    dataButton.classList.add('slide-left');
+    dadButton.classList.add('slide-right');
+    removeClass(closeButton, 'hide');
+    closeButton.classList.add('show');
+  }
+
+  function dataMenu() {
+    console.log('the data button was clicked');
+    showDataPods();
+    dataButton.classList.add('fade-out');
+    devButton.classList.add('slide-left');
+    dadButton.classList.add('slide-right');
+    closeButton.classList.remove('hide');
+    closeButton.classList.add('show');
+  }
+
+  function dadMenu() {
+    console.log('the dad button was clicked');
+    showDadPods();
+    dadButton.classList.add('fade-out');
+    devButton.classList.add('slide-left');
+    dataButton.classList.add('slide-right');
+    closeButton.classList.remove('hide');
+    closeButton.classList.add('show');
+  }
+
+  // SKILL POD FUNCTIONS //
+
+  // DEVELOPER //
+
+  function showDevPods() {
+    for (var _i = 0; _i < devPods.length; _i++) {
+      removeClass(devPods[_i], 'hide');
+      addClass(devPods[_i], 'show');
     }
 
-    function hideDevPods() {
+    addClass(mainPod, 'show');
+    removeClass(mainPod, 'hide');
+    mainPodText.innerHTML = 'DEV';
+    pod1Text.innerHTML = 'HTML';
+    pod2Text.innerHTML = 'CSS';
+    pod3Text.innerHTML = 'JS';
+    pod4Text.innerHTML = 'GIT';
+    pod5Text.innerHTML = 'REACT';
+    pod6Text.innerHTML = 'LINUX';
+  }
 
-      for (let i = 0; i < devPods.length; i++) {
-        addClass(devPods[i], 'hide');
-        removeClass(devPods[i], 'show');
-      }
-      addClass(mainPod, 'hide');
-      removeClass(mainPod, 'show');
+  function hideDevPods() {
+    for (var _i2 = 0; _i2 < devPods.length; _i2++) {
+      addClass(devPods[_i2], 'hide');
+      removeClass(devPods[_i2], 'show');
     }
 
-    // DATA //
+    addClass(mainPod, 'hide');
+    removeClass(mainPod, 'show');
+  }
 
-    function showDataPods() {
+  // DATA //
 
-      for (let i = 0; i < dataPods.length; i++) {
-        removeClass(dataPods[i], 'hide');
-        addClass(dataPods[i], 'show');
-      }
-
-      addClass(mainPod, 'show')
-      removeClass(mainPod, 'hide');
-      mainPodText.innerHTML = 'DATA';
-
-      pod1Text.innerHTML = 'PYTHON';
-      pod3Text.innerHTML = 'SQL';
-      pod4Text.innerHTML = 'D3';
-
-    }
-    function hideDataPods() {
-
-      for (let i = 0; i < dataPods.length; i++) {
-        addClass(dataPods[i], 'hide');
-        removeClass(dataPods[i], 'show');
-      }
-      addClass(mainPod, 'hide');
-      removeClass(mainPod, 'show');
+  function showDataPods() {
+    for (var _i3 = 0; _i3 < dataPods.length; _i3++) {
+      removeClass(dataPods[_i3], 'hide');
+      addClass(dataPods[_i3], 'show');
     }
 
-    // DAD //
+    addClass(mainPod, 'show');
+    removeClass(mainPod, 'hide');
+    mainPodText.innerHTML = 'DATA';
+    pod1Text.innerHTML = 'PYTHON';
+    pod3Text.innerHTML = 'SQL';
+    pod4Text.innerHTML = 'D3';
+  }
 
-    function showDadPods() {
-
-      for (let i = 0; i < dadPods.length; i++) {
-        removeClass(dadPods[i], 'hide');
-        addClass(dadPods[i], 'show');
-      }
-      addClass(mainPod, 'show')
-      removeClass(mainPod, 'hide');
-      mainPodText.innerHTML = 'DAD';
-
-      pod1Text.innerHTML = 'LEGOS';
-      pod2Text.innerHTML = 'HOCKEY';
-      pod4Text.innerHTML = 'FORTS';
-      pod5Text.innerHTML = 'PIZZA';
+  function hideDataPods() {
+    for (var _i4 = 0; _i4 < dataPods.length; _i4++) {
+      addClass(dataPods[_i4], 'hide');
+      removeClass(dataPods[_i4], 'show');
     }
 
-    function hideDadPods() {
+    addClass(mainPod, 'hide');
+    removeClass(mainPod, 'show');
+  }
 
-      for (let i = 0; i < dadPods.length; i++) {
-        addClass(dadPods[i], 'hide');
-        removeClass(dadPods[i], 'show');
-      }
-      addClass(mainPod, 'hide');
-      removeClass(mainPod, 'show');
+  // DAD //
+
+
+  function showDadPods() {
+    for (var _i5 = 0; _i5 < dadPods.length; _i5++) {
+      removeClass(dadPods[_i5], 'hide');
+      addClass(dadPods[_i5], 'show');
     }
 
+    addClass(mainPod, 'show');
+    removeClass(mainPod, 'hide');
+    mainPodText.innerHTML = 'DAD';
+    pod1Text.innerHTML = 'LEGOS';
+    pod2Text.innerHTML = 'HOCKEY';
+    pod4Text.innerHTML = 'FORTS';
+    pod5Text.innerHTML = 'PIZZA';
+  }
 
-    // CLOSE BUTTON FUNTCTIONS //
-
-    closeButton.onclick = function() {
-        if (devClicked == true) {
-            hideDevPods();
-            devButton.classList.remove('fade-out');
-            dataButton.classList.remove('slide-left');
-            dadButton.classList.remove('slide-right');
-            closeButton.classList.remove('show');
-            closeButton.classList.add('hide');
-            // removeClass(devSkill, 'draw-grid');
-            devClicked = false;
-        } else if (dataClicked == true) {
-            hideDataPods();
-            dataButton.classList.remove('fade-out');
-            devButton.classList.remove('slide-left');
-            dadButton.classList.remove('slide-right');
-            closeButton.classList.remove('show');
-            closeButton.classList.add('hide');
-            // removeClass(dataSkill, 'draw-grid');
-            dataClicked = false;
-        } else if (dadClicked == true) {
-            hideDadPods();
-            removeClass(dadButton, 'fade-out')
-            removeClass(devButton, 'slide-left');
-            removeClass(dataButton, 'slide-right');
-            removeClass(closeButton, 'show');
-            addClass(closeButton, 'hide');
-            // removeClass(dadSkill, 'draw-grid');
-            dadClicked = false;
-        }
+  function hideDadPods() {
+    for (var _i6 = 0; _i6 < dadPods.length; _i6++) {
+      addClass(dadPods[_i6], 'hide');
+      removeClass(dadPods[_i6], 'show');
     }
 
+    addClass(mainPod, 'hide');
+    removeClass(mainPod, 'show');
+  }
 
+  // CLOSE BUTTON FUNTCTIONS //
 
-}
+  closeButton.onclick = function () {
+    if (devClicked == true) {
+      hideDevPods();
+      devButton.classList.remove('fade-out');
+      dataButton.classList.remove('slide-left');
+      dadButton.classList.remove('slide-right');
+      closeButton.classList.remove('show');
+      closeButton.classList.add('hide');
+
+      devClicked = false;
+    } else if (dataClicked == true) {
+      hideDataPods();
+      dataButton.classList.remove('fade-out');
+      devButton.classList.remove('slide-left');
+      dadButton.classList.remove('slide-right');
+      closeButton.classList.remove('show');
+      closeButton.classList.add('hide');
+
+      dataClicked = false;
+    } else if (dadClicked == true) {
+      hideDadPods();
+      removeClass(dadButton, 'fade-out');
+      removeClass(devButton, 'slide-left');
+      removeClass(dataButton, 'slide-right');
+      removeClass(closeButton, 'show');
+      addClass(closeButton, 'hide');
+
+      dadClicked = false;
+    }
+  };
+};
